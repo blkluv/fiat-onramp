108b9a9443ac4ea91f69f93e3a7f302329979ee1
Sweets Sweetman <sweetmantech@gmail.com>
2023-02-22 12:30:49 -0300

    I init child components <ResultsForm /> & <VerifyForm />

--- components/DeployWrapperForm/DeployWrapperForm.tsx
+++ components/DeployWrapperForm/DeployWrapperForm.tsx
@@ -1,88 +1,30 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import axios, { AxiosRequestConfig } from "axios";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import { allChains } from "wagmi";
+import { useCallback, useState } from "react";
 import shortenAddress from "../../lib/shortenAddress";
-import Button from "../Button";
 import DeployWrapperButton from "../DeployWrapperButton";
-import Spinner from "../Spinner";
+import ResultsForm from "../ResultsForm";
+import VerifyForm from "../VerifyForm";
 
 const DeployWrapperForm = ({ publicKey, setDeploymentStep }: any) => {
   const [contractAddress, setContractAddress] = useState("");
   const [matchingContracts, setMatchingContracts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-
-  const onClick = async (e: any) => {
-    setLoading(true);
-    e.preventDefault();
-    console.log("contract address", contractAddress);
-    const { data } = await axios.get("/api/getContractInfo", {
-      params: { contractAddress },
-    } as AxiosRequestConfig);
-    const { matches } = data;
-    console.log("matches", matches);
-    setMatchingContracts(matches);
-    setLoading(false);
-  };
-
-  const handleChange = (e: any) => {
-    setContractAddress(e.target.value);
-  };
 
   return (
     <main
       style={{ backgroundImage: "url('/images/bg.png')" }}
       className="w-screen min-h-screen bg-cover"
     >
-      <form
-        className="flex flex-col justify-center items-center gap-10 w-[90vw] h-[85vh] "
-        onSubmit={onClick}
-      >
+      <div className="flex flex-col justify-center items-center gap-10 w-[90vw] h-[85vh] ">
         {matchingContracts.length > 0 ? (
-          <div className="flex flex-col gap-5">
-            <div className="text-center text-xl">
-              Results for {shortenAddress(contractAddress)}
-            </div>
-            {matchingContracts.map((contract: any) => {
-              const myChain = allChains.find(
-                (chain) => contract.chainId === chain.id
-              );
-              return (
-                <Button key={contract?.name}>
-                  {contract?.name} - {myChain?.name}
-                </Button>
-              );
-            })}
-            <Button onClick={() => setMatchingContracts([])}>try again</Button>
-
-            <Button
-              onClick={(e: any) => router.push("https://twitter.com/decentxyz")}
-            >
-              get help
-            </Button>
-          </div>
+          <ResultsForm
+            contracts={matchingContracts}
+            address={contractAddress}
+            reset={() => setMatchingContracts([])}
+          />
         ) : (
-          <div className="flex flex-col justify-center items-center gap-10">
-            <div>
-              <p className="font-header">Your Contract Address</p>
-              <input
-                className="border border-black text-black rounded-lg p-4 w-[400px]"
-                onChange={handleChange}
-              />
-            </div>
-            <Button onClick={onClick} type="submit" disabled={loading}>
-              {loading ? (
-                <Spinner width={20} height={20} color="black" />
-              ) : (
-                "Verify"
-              )}
-            </Button>
-          </div>
+          <VerifyForm onSuccess={setMatchingContracts} />
         )}
-      </form>
+      </div>
       <div className="min-h-screen grid md:grid-cols-2 grid-cols-1">
         <div className="flex flex-wrap items-center">
           <div className="space-y-8">
